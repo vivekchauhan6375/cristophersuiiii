919cf610047abd15024f7bd895c55340d08639dc
vivekchauhan6375 <vp637558@gmail.com>
2024-06-04 10:43:31 +0530

    Project code and added train_pipeline.py

--- src/config/config.py
+++ src/config/config.py
@@ -3,28 +3,16 @@ import pathlib
 import src
 
 NUM_INPUTS = 2
-NUM_LAYERS = 3 
-P = [NUM_LAYERS, 2, 1]
+NUM_LAYERS = 3
+P = [NUM_INPUTS,2,1]
 
-f_dict = [None , 'Linear', 'sigmoid ']
-LOSS_FUNCTION = 'MEAN SQUARED ERROR'
+f = [None,"linear","sigmoid"]
+
+LOSS_FUNCTION = "Mean Squared Error"
 MINI_BATCH_SIZE = 1
-PACKAGE_ROOT = pathlib.pATH(src.__FILE__).resolve().parent
-DATAPATH = os.path.join(PACKAGE_ROOT,'datasets')
+
+PACKAGE_ROOT = pathlib.Path(src.__file__).resolve().parent
+DATAPATH = os.path.join(PACKAGE_ROOT,"datasets") 
 #"/src/datasets"
 SAVED_MODEL_PATH = os.path.join(PACKAGE_ROOT,"trained_models")
 #"/src/trained_models"
-# theta0 = [None]
-# theta = [None]
-
-# z = [None]*NUM_LAYERS
-# h = [None]*NUM_LAYERS
-
-'''
-del_fl_by_del_z = [None]*NUM_LAYERS
-del_hl_by_del_theta0 = [None]*NUM_LAYERS
-del_hl_by_del_theta = [None]*NUM_LAYERS
-del_L_by_del_h = [None]*NUM_LAYERS
-del_L_by_del_theta0 = [None]*NUM_LAYERS
-del_L_by_del_theta = [None]*NUM_LAYERS
-'''
